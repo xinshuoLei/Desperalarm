230f23d48695a26cee528be2de1b465bf38d0347
realBJ-dot <peiyuan3@illinois.edu>
2021-11-01 00:46:20 -0500

    almost

---
+++ DesperAlarrrrrrm/app/src/main/java/com/example/desperalarm/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.desperalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button regularAlarm = findViewById(R.id.RegularAlarm);
+        regularAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RegularAlarm.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+}
+
---
+++ DesperatAlarm/app/src/main/java/com/example/desperatalarm/AlarmReciever.java
@@ -0,0 +1,2 @@
+package com.example.desperatalarm;public class AlarmReciever {
+}
