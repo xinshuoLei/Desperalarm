adf25524a367a4bf67f8eff8a096a6d07dbf9846
Lei, Xinshuo <xinshuo3@illinois.edu>
2021-11-01 22:40:44 -0500

    Update DesperAlarm.java

--- DesperAlarrrrrrm/app/src/main/java/com/example/desperalarm/DesperAlarm.java
+++ DesperAlarrrrrrm/app/src/main/java/com/example/desperalarm/DesperAlarm.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+/**
+* The screen for desperalarm. to be implemented
+*/
 public class DesperAlarm extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
