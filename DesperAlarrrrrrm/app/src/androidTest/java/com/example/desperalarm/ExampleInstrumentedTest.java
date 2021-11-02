d689192cfa6b846ef8eb6b6f8cc42f10ce92bbf7
realBJ-dot <peiyuan3@illinois.edu>
2021-11-01 22:35:50 -0500

    updated screenshots test

--- DesperAlarrrrrrm/app/src/androidTest/java/com/example/desperalarm/ExampleInstrumentedTest.java
+++ DesperAlarrrrrrm/app/src/androidTest/java/com/example/desperalarm/ExampleInstrumentedTest.java
@@ -29,30 +29,43 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.alarm_setter", appContext.getPackageName());
     }
     @Test
-    public void doScreenshot() {
-        /*
-         * Create and set up your view some how. This might be inflating,
-         * or creating from a view class. You might want to set properties
-         * on the view.
-         */
+    public void doDesperateScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.desperalarm, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doRegularScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.regular_alarm, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doHelpScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.help, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doTimerScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.timer, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doMainScreenshot() {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.activity_main, null, false);
-
-        /*
-         * Measure and layout the view. In this example we give an exact
-         * width but all the height to be WRAP_CONTENT.
-         */
-        ViewHelpers.setupView(view)
-                .setExactWidthDp(300)
-                .layout();
-
-        /*
-         * Take the actual screenshot. At the end of this call the screenshot
-         * is stored on the device, and the gradle plugin takes care of
-         * pulling it and displaying it to you in nice ways.
-         */
-        Screenshot.snap(view)
-                .record();
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
     }
 }
