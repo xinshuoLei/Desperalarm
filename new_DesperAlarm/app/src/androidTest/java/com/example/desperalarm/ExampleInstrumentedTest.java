17464657d1a275ca925d0e44a1c3c65fae26d58c
realBJ-dot <peiyuan3@illinois.edu>
2021-11-15 21:07:25 -0600

    alll done

--- new_DesperAlarm/app/src/androidTest/java/com/example/desperalarm/ExampleInstrumentedTest.java
+++ new_DesperAlarm/app/src/androidTest/java/com/example/desperalarm/ExampleInstrumentedTest.java
@@ -18,12 +18,75 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-
     @Test
-    public void doDesperateScreenshot() {
+    public void doAboutScreenshot() {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.about, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doDesperateQuestionScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.activity_question, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doRingScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.activity_ring, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doCreateAlarmScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.fragment_createalarm, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doListAlarmScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.fragment_listalarms, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doStartScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.fragment_start, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doItemAlarmScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_alarm, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doRegularAlarmGuideScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.regular_alarm_guide, null, false);
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout();
+        Screenshot.snap(view).record();
+    }
+    @Test
+    public void doTimerScreenshot() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.timer, null, false);
         ViewHelpers.setupView(view).setExactWidthDp(300).layout();
         Screenshot.snap(view).record();
     }
